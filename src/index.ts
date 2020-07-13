def662355f61a70dade369a24aa4533b5ba04a40
Ollie Husband <ollie.husband@ktech.com>
2020-07-13 16:06:31 +0100

    Typo in root file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import app from './app';
 import path from "path";
 import Parloa from "./models/Parloa";
 import Logger from "./util/Logger";
+import CustomerFileParser from './util/CustomerFileParser';
+
 const parloa = new Parloa();
 const logger = new Logger(console.log);
 const customerFileParser = new CustomerFileParser(path.join(__dirname, '..', '/customer.txt'), parloa, logger);
